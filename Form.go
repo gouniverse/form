0f4d76d41d826571742d4a678bbac09e2cb5a79d
Milan Lesichkov <lesichkovm@gmail.com>
2023-09-12 08:40:15 +0100

    Create Form.go

---
+++ Form.go
@@ -0,0 +1,81 @@
+package form
+
+import (
+	"net/http"
+
+	"github.com/gouniverse/hb"
+)
+
+type Form struct {
+	id             string
+	className      string
+	fields         []Field
+	fileManagerURL string
+	method         string
+	actionUrl      string
+}
+
+type FormOptions struct {
+	ActionURL      string  // optional
+	ClassName      string  // optional
+	ID             string  // optional
+	Fields         []Field // optional
+	FileManagerURL string  // optional
+	Method         string  // optional
+}
+
+func NewForm(opts FormOptions) *Form {
+	form := &Form{}
+	form.fields = opts.Fields
+	form.fileManagerURL = opts.FileManagerURL
+	form.method = opts.Method
+	if form.method == "" {
+		form.method = http.MethodPost
+	}
+	form.actionUrl = opts.ActionURL
+	form.id = opts.ID
+	form.className = opts.ClassName
+	return form
+}
+
+func (form *Form) AddField(field Field) {
+	form.fields = append(form.fields, field)
+}
+
+func (form *Form) GetFields() []Field {
+	return form.fields
+}
+
+func (form *Form) SetFields(fields []Field) {
+	form.fields = fields
+}
+
+func (form *Form) GetFileManagerURL() string {
+	return form.fileManagerURL
+}
+
+func (form *Form) SetFileManagerURL(url string) {
+	form.fileManagerURL = url
+}
+
+func (form *Form) Build() *hb.Tag {
+	tags := []*hb.Tag{}
+
+	for _, field := range form.fields {
+		tags = append(tags, field.BuildFormGroup(form.fileManagerURL))
+	}
+
+	hbForm := hb.NewForm()
+	hbForm.Children(tags)
+	hbForm.Method(form.method)
+	if form.actionUrl != "" {
+		hbForm.Action(form.actionUrl)
+	}
+	if form.id != "" {
+		hbForm.ID(form.id)
+	}
+	if form.className != "" {
+		hbForm.Class(form.className)
+	}
+	return hbForm
+}
