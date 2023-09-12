2dc99b7a8052bb0108ae7079f2c232f6c09584fd
Milan Lesichkov <lesichkovm@gmail.com>
2023-09-12 08:38:35 +0100

    Create consts.go

---
+++ consts.go
@@ -0,0 +1,13 @@
+package form
+
+const FORM_FIELD_TYPE_DATE = "date"
+const FORM_FIELD_TYPE_DATETIME = "datetime"
+const FORM_FIELD_TYPE_IMAGE = "image"
+const FORM_FIELD_TYPE_HTMLAREA = "htmlarea"
+const FORM_FIELD_TYPE_HIDDEN = "hidden"
+const FORM_FIELD_TYPE_NUMBER = "number"
+const FORM_FIELD_TYPE_PASSWORD = "password"
+const FORM_FIELD_TYPE_SELECT = "select"
+const FORM_FIELD_TYPE_STRING = "string"
+const FORM_FIELD_TYPE_TEXTAREA = "textarea"
+const FORM_FIELD_TYPE_RAW = "raw"
