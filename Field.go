fbf22269823b1da62a642a3173bbdc6418792214
Milan Lesichkov <lesichkovm@gmail.com>
2023-09-14 07:41:04 +0100

    Update Field.go

--- Field.go
+++ Field.go
@@ -1,21 +1,34 @@
 package form
 
 import (
+	"strconv"
+
 	"github.com/gouniverse/bs"
 	"github.com/gouniverse/hb"
 )
 
 type Field struct {
-	Type     string
-	Name     string
-	Label    string
-	Help     string
-	Options  []FieldOption
-	OptionsF func() []FieldOption
-	Value    string
-	Required bool
-	Readonly bool
-	Disabled bool
+	Type         string
+	Name         string
+	Label        string
+	Help         string
+	Options      []FieldOption
+	OptionsF     func() []FieldOption
+	Value        string
+	Required     bool
+	Readonly     bool
+	Disabled     bool
+	TableOptions TableOptions
+}
+
+type TableColumn struct {
+	Label string
+	Width int
+}
+type TableOptions struct {
+	Header     []TableColumn
+	Rows       [][]Field
+	Repeatable bool
 }
 
 func (field *Field) IsDate() bool {
@@ -54,6 +67,10 @@ func (field *Field) IsString() bool {
 	return field.Type == FORM_FIELD_TYPE_STRING
 }
 
+func (field *Field) IsTable() bool {
+	return field.Type == FORM_FIELD_TYPE_TABLE
+}
+
 func (field *Field) IsTextArea() bool {
 	return field.Type == FORM_FIELD_TYPE_TEXTAREA
 }
@@ -72,6 +89,152 @@ func (field *Field) IsRequired() bool {
 
 func (field *Field) IsRaw() bool {
 	return field.Type == FORM_FIELD_TYPE_RAW
+}
+
+func (field *Field) fieldInput(fileManagerURL string) *hb.Tag {
+	input := hb.NewTag(``) // no tag by default
+
+	if field.IsDate() ||
+		field.IsHidden() ||
+		field.IsPassword() ||
+		field.IsString() ||
+		field.IsNumber() {
+
+		input = hb.NewInput().
+			Class("form-control").
+			Name(field.Name).
+			Value(field.Value)
+
+		if field.IsDate() {
+			input.Type(hb.TYPE_DATE)
+		}
+
+		if field.IsHidden() {
+			input.Type(hb.TYPE_HIDDEN)
+		}
+
+		if field.IsNumber() {
+			input.Type(hb.TYPE_NUMBER)
+		}
+
+		if field.IsPassword() {
+			input.Type(hb.TYPE_PASSWORD)
+		}
+
+		if field.IsString() {
+			input.Type(hb.TYPE_TEXT)
+		}
+	}
+
+	if field.IsDateTime() {
+		input = hb.NewInput().
+			Type(hb.TYPE_DATETIME).
+			Class("form-control").
+			Name(field.Name).
+			Value(field.Value)
+		// formGroupInput = hb.NewTag(`el-date-picker`).Attr("type", "datetime").Attr("v-model", "entityModel."+fieldName)
+		// formGroupInput = hb.NewTag(`n-date-picker`).Attr("type", "datetime").Class("form-control").Attr("v-model", "entityModel."+fieldName)
+	}
+
+	if field.IsImage() {
+		input = hb.NewDiv().Children([]*hb.Tag{
+			hb.NewImage().
+				AttrIf(field.Value != "", `src`, field.Value).
+				AttrIf(field.Value == "", `src`, `https://www.freeiconspng.com/uploads/no-image-icon-11.PNG`).
+				Style(`width:200px;`),
+
+			bs.InputGroup().Children([]*hb.Tag{
+				hb.NewInput().
+					Type(hb.TYPE_URL).
+					Class("form-control").
+					Name(field.Name).
+					Value(field.Value),
+				hb.If(fileManagerURL != "", bs.InputGroupText().Children([]*hb.Tag{
+					hb.NewHyperlink().HTML("Browse").Href(fileManagerURL).Target("_blank"),
+				})),
+			}),
+		})
+	}
+
+	if field.IsHtmlArea() {
+		input = hb.NewTag("trumbowyg").
+			Name(field.Name).
+			Attr("v-model", "entityModel."+field.Name).
+			Attr(":config", "trumbowigConfig").
+			Class("form-control")
+	}
+
+	if field.IsSelect() {
+		input = hb.NewSelect().
+			Name(field.Name).
+			Class("form-select")
+
+		for _, opt := range field.Options {
+			option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
+			option.AttrIf(field.Value == opt.Key, "selected", "selected")
+			input.AddChild(option)
+		}
+		if field.OptionsF != nil {
+			for _, opt := range field.OptionsF() {
+				option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
+				option.AttrIf(field.Value == opt.Key, "selected", "selected")
+				input.AddChild(option)
+			}
+		}
+	}
+
+	if field.IsTable() {
+		header := hb.NewThead()
+		for _, v := range field.TableOptions.Header {
+			th := hb.NewTH().HTML(v.Label)
+			if v.Width != 0 {
+				th.Style("width:" + strconv.Itoa(v.Width) + "px")
+			}
+			header.AddChild(th)
+		}
+		rows := hb.NewTbody()
+		for _, rowFields := range field.TableOptions.Rows {
+			tr := hb.NewTR()
+			for _, rowField := range rowFields {
+				td := hb.NewTD().Child(rowField.fieldInput(fileManagerURL))
+				tr.AddChild(td)
+			}
+			rows.AddChild(tr)
+		}
+		input = hb.NewTable().
+			Class("table table-striped table-hover mb-0").
+			Child(header).
+			Child(rows)
+	}
+
+	if field.IsTextArea() {
+		input = hb.NewTextArea().
+			Name(field.Name).
+			Class("form-control").
+			HTML(field.Value)
+	}
+
+	if field.IsReadonly() {
+		// Selects are different. Readonly for selects does not work.
+		// Disable and create a hidden field
+		if field.IsSelect() {
+			input.Attr("disabled", "disabled")
+			input.Name(field.Name + "_Readonly")
+			// hiddenInput = hb.NewInput().
+			// 	Class("form-control").
+			// 	Name(field.Name).
+			// 	Value(field.Value).
+			// 	Type(hb.TYPE_HIDDEN)
+		} else {
+			input.Attr("readonly", "readonly")
+		}
+	}
+
+	if field.IsDisabled() {
+		input.Attr("disabled", "disabled")
+	}
+
+	return input
 }
 
 func (field *Field) BuildFormGroup(fileManagerURL string) *hb.Tag {
@@ -95,128 +258,21 @@ func (field *Field) BuildFormGroup(fileManagerURL string) *hb.Tag {
 			hb.NewSup().HTML("*").Class("text-danger ml-1"),
 		)
 
-	formGroupInput := hb.NewTag(``) // no tag by default
-	hiddenInput := hb.NewTag(``)    // special case, no tag by default
+	// Hidden input
+	hiddenInput := hb.NewTag(``) // special case, no tag by default
 
-	if field.IsDate() ||
-		field.IsHidden() ||
-		field.IsPassword() ||
-		field.IsString() ||
-		field.IsNumber() {
-		formGroupInput = hb.NewInput().
+	if field.IsReadonly() && field.IsSelect() {
+		hiddenInput = hb.NewInput().
 			Class("form-control").
-			Name(fieldName).
-			Value(field.Value)
-
-		if field.IsDate() {
-			formGroupInput.Type(hb.TYPE_DATE)
-		}
-
-		if field.IsHidden() {
-			formGroupInput.Type(hb.TYPE_HIDDEN)
-		}
-
-		if field.IsNumber() {
-			formGroupInput.Type(hb.TYPE_NUMBER)
-		}
-
-		if field.IsPassword() {
-			formGroupInput.Type(hb.TYPE_PASSWORD)
-		}
-
-		if field.IsString() {
-			formGroupInput.Type(hb.TYPE_TEXT)
-		}
-	}
-
-	if field.IsDateTime() {
-		formGroupInput = hb.NewInput().
-			Type(hb.TYPE_DATETIME).
-			Class("form-control").
-			Name(fieldName).
-			Value(field.Value)
-		// formGroupInput = hb.NewTag(`el-date-picker`).Attr("type", "datetime").Attr("v-model", "entityModel."+fieldName)
-		// formGroupInput = hb.NewTag(`n-date-picker`).Attr("type", "datetime").Class("form-control").Attr("v-model", "entityModel."+fieldName)
-	}
-
-	if field.IsImage() {
-		formGroupInput = hb.NewDiv().Children([]*hb.Tag{
-			hb.NewImage().
-				AttrIf(field.Value != "", `src`, field.Value).
-				AttrIf(field.Value == "", `src`, `https://www.freeiconspng.com/uploads/no-image-icon-11.PNG`).
-				Style(`width:200px;`),
-
-			bs.InputGroup().Children([]*hb.Tag{
-				hb.NewInput().
-					Type(hb.TYPE_URL).
-					Class("form-control").
-					Name(fieldName).
-					Value(field.Value),
-				hb.If(fileManagerURL != "", bs.InputGroupText().Children([]*hb.Tag{
-					hb.NewHyperlink().HTML("Browse").Href(fileManagerURL).Target("_blank"),
-				})),
-			}),
-		})
-	}
-
-	if field.IsHtmlArea() {
-		formGroupInput = hb.NewTag("trumbowyg").
-			Name(fieldName).
-			Attr("v-model", "entityModel."+fieldName).
-			Attr(":config", "trumbowigConfig").
-			Class("form-control")
-	}
-
-	if field.IsSelect() {
-		formGroupInput = hb.NewSelect().
-			Name(fieldName).
-			Class("form-select")
-
-		for _, opt := range field.Options {
-			option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
-			option.AttrIf(field.Value == opt.Key, "selected", "selected")
-			formGroupInput.AddChild(option)
-		}
-		if field.OptionsF != nil {
-			for _, opt := range field.OptionsF() {
-				option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
-				option.AttrIf(field.Value == opt.Key, "selected", "selected")
-				formGroupInput.AddChild(option)
-			}
-		}
-	}
-
-	if field.IsTextArea() {
-		formGroupInput = hb.NewTextArea().
-			Name(fieldName).
-			Class("form-control").
-			HTML(field.Value)
-	}
-
-	if field.IsReadonly() {
-		// Selects are different. Readonly for selects does not work.
-		// Disable and create a hidden field
-		if field.IsSelect() {
-			formGroupInput.Attr("disabled", "disabled")
-			formGroupInput.Name(field.Name + "_Readonly")
-			hiddenInput = hb.NewInput().
-				Class("form-control").
-				Name(field.Name).
-				Value(field.Value).
-				Type(hb.TYPE_HIDDEN)
-		} else {
-			formGroupInput.Attr("readonly", "readonly")
-		}
-	}
-
-	if field.IsDisabled() {
-		formGroupInput.Attr("disabled", "disabled")
+			Name(field.Name).
+			Value(field.Value).
+			Type(hb.TYPE_HIDDEN)
 	}
 
 	if !field.IsHidden() {
 		formGroup.Child(formGroupLabel)
 	}
-	formGroup.Child(formGroupInput)
+	formGroup.Child(field.fieldInput(fileManagerURL))
 	formGroup.Child(hiddenInput)
 
 	// Add help
