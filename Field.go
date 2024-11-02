90c7a72cf9dec1e90d1b873714499f95108d1e2e
Sinevia <info@sinevia.com>
2024-11-02 20:01:15 +0000

    updated image field

--- Field.go
+++ Field.go
@@ -3,7 +3,6 @@ package form
 import (
 	"strconv"
 
-	"github.com/gouniverse/bs"
 	"github.com/gouniverse/hb"
 	"github.com/gouniverse/uid"
 	"github.com/gouniverse/utils"
@@ -153,135 +152,31 @@ func (field *Field) fieldInput(fileManagerURL string) *hb.Tag {
 	}
 
 	if field.IsDateTime() {
-		input = hb.NewInput().
-			ID(field.ID).
-			Type(hb.TYPE_DATETIME).
-			Class("form-control").
-			Name(field.Name).
-			Value(field.Value)
-		// formGroupInput = hb.NewTag(`el-date-picker`).Attr("type", "datetime").Attr("v-model", "entityModel."+fieldName)
-		// formGroupInput = hb.NewTag(`n-date-picker`).Attr("type", "datetime").Class("form-control").Attr("v-model", "entityModel."+fieldName)
+		input = field.fieldDateTime()
 	}
 
 	if field.IsImage() {
-		input = hb.NewDiv().Children([]hb.TagInterface{
-			hb.NewImage().
-				AttrIf(field.Value != "", `src`, field.Value).
-				AttrIf(field.Value == "", `src`, `https://www.freeiconspng.com/uploads/no-image-icon-11.PNG`).
-				Style(`width:200px;`),
-
-			bs.InputGroup().Children([]hb.TagInterface{
-				hb.NewInput().
-					ID(field.ID).
-					Type(hb.TYPE_URL).
-					Class("form-control").
-					Name(field.Name).
-					Value(field.Value),
-				hb.If(fileManagerURL != "", bs.InputGroupText().Children([]hb.TagInterface{
-					hb.NewHyperlink().HTML("Browse").Href(fileManagerURL).Target("_blank"),
-				})),
-			}),
-		})
+		input = field.fieldImage(fileManagerURL)
 	}
 
 	if field.IsHtmlArea() {
-		input = hb.NewWrap().Child(
-			hb.NewTextArea().
-				ID(field.ID).
-				Class("form-control").
-				Name(field.Name).
-				Text(field.Value)).
-			Child(hb.NewScript(field.TrumbowygScript()))
-		// Child(hb.NewScript(`setTimeout(() => {initWysiwyg("` + field.ID + `")}, 100);`))
+		input = field.fieldHtmlArea()
 	}
 
 	if field.IsBlockEditor() {
-		textInputOnError := hb.NewTextArea().
-			ID(field.ID).
-			Class("form-control").
-			Name(field.Name).
-			Text(field.Value)
-
-		if field.CustomInput == nil {
-			return hb.Wrap().
-				Child(hb.Div().
-					Class("alert alert-danger").
-					Text("CustomInput is nil")).
-				Child(textInputOnError)
-		}
-
-		input = hb.Wrap(field.CustomInput)
+		input = field.fieldBlockEditor()
 	}
 
 	if field.IsSelect() {
-		input = hb.NewSelect().
-			ID(field.ID).
-			Name(field.Name).
-			Class("form-select")
-
-		for _, opt := range field.Options {
-			option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
-			option.AttrIf(field.Value == opt.Key, "selected", "selected")
-			input.AddChild(option)
-		}
-		if field.OptionsF != nil {
-			for _, opt := range field.OptionsF() {
-				option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
-				option.AttrIf(field.Value == opt.Key, "selected", "selected")
-				input.AddChild(option)
-			}
-		}
+		input = field.fieldSelect()
 	}
 
 	if field.IsTable() {
-		header := hb.NewThead()
-		if field.TableOptions.RowDeleteButton != nil {
-			th := hb.NewTH().HTML("#").Style("width:1px;")
-			header.AddChild(th)
-		}
-		for _, v := range field.TableOptions.Header {
-			th := hb.NewTH().HTML(v.Label)
-			if v.Width != 0 {
-				th.Style("width:" + strconv.Itoa(v.Width) + "px")
-			}
-			header.AddChild(th)
-		}
-
-		rows := hb.NewTbody()
-		for rowIndex, rowFields := range field.TableOptions.Rows {
-			tr := hb.NewTR().Data("row-index", utils.ToString(rowIndex))
-			if field.TableOptions.RowDeleteButton != nil {
-				deleteButton := field.TableOptions.RowDeleteButton.
-					Type(hb.TYPE_BUTTON).
-					Data("row-index", utils.ToString(rowIndex))
-				td := hb.NewTH().Child(deleteButton)
-				tr.AddChild(td)
-			}
-			for _, rowField := range rowFields {
-				td := hb.NewTD().Child(rowField.fieldInput(fileManagerURL))
-				tr.AddChild(td)
-			}
-			rows.AddChild(tr)
-		}
-		table := hb.NewTable().
-			ID(field.ID).
-			Class("table table-striped table-hover mb-0").
-			Child(header).
-			Child(rows)
-
-		input = hb.NewWrap().Child(table)
-
-		if field.TableOptions.RowAddButton != nil {
-			input.AddChild(hb.NewDiv().Child(field.TableOptions.RowAddButton.Type(hb.TYPE_BUTTON)))
-		}
+		input = field.fieldTable(fileManagerURL)
 	}
 
 	if field.IsTextArea() {
-		input = hb.NewTextArea().
-			ID(field.ID).
-			Name(field.Name).
-			Class("form-control").
-			HTML(field.Value)
+		input = field.fieldTextArea()
 	}
 
 	if field.IsReadonly() {
@@ -290,11 +185,6 @@ func (field *Field) fieldInput(fileManagerURL string) *hb.Tag {
 		if field.IsSelect() {
 			input.Attr("disabled", "disabled")
 			input.Name(field.Name + "_Readonly")
-			// hiddenInput = hb.NewInput().
-			// 	Class("form-control").
-			// 	Name(field.Name).
-			// 	Value(field.Value).
-			// 	Type(hb.TYPE_HIDDEN)
 		} else {
 			input.Attr("readonly", "readonly")
 		}
@@ -305,6 +195,164 @@ func (field *Field) fieldInput(fileManagerURL string) *hb.Tag {
 	}
 
 	return input
+}
+
+func (field *Field) fieldBlockEditor() *hb.Tag {
+	textInputOnError := hb.NewTextArea().
+		ID(field.ID).
+		Class("form-control").
+		Name(field.Name).
+		Text(field.Value)
+
+	if field.CustomInput == nil {
+		return hb.Wrap().
+			Child(hb.Div().
+				Class("alert alert-danger").
+				Text("CustomInput is nil")).
+			Child(textInputOnError)
+	}
+
+	return hb.Wrap(field.CustomInput)
+}
+
+func (field *Field) fieldDateTime() *hb.Tag {
+	input := hb.NewInput().
+		ID(field.ID).
+		Type(hb.TYPE_DATETIME).
+		Class("form-control").
+		Name(field.Name).
+		Value(field.Value)
+	// formGroupInput = hb.NewTag(`el-date-picker`).Attr("type", "datetime").Attr("v-model", "entityModel."+fieldName)
+	// formGroupInput = hb.NewTag(`n-date-picker`).Attr("type", "datetime").Class("form-control").Attr("v-model", "entityModel."+fieldName)
+
+	return input
+}
+
+func (field *Field) fieldImage(fileManagerURL string) *hb.Tag {
+	image := hb.NewImage().
+		Class(`img-fluid rounded-start`).
+		Style(`margin-bottom: 15px;`).
+		AttrIf(field.Value != "", `src`, field.Value).
+		AttrIf(field.Value == "", `src`, `https://www.freeiconspng.com/uploads/no-image-icon-11.PNG`).
+		Style(`width:100%;max-height:100px;`)
+
+	textArea := hb.NewTextArea().
+		ID(field.ID).
+		Type(hb.TYPE_TEXT).
+		Class("form-control").
+		Style(`height:70px;`).
+		Name(field.Name).
+		Text(field.Value).
+		Placeholder(field.Placeholder)
+
+	fileManagerLink := lo.If(fileManagerURL != "", hb.NewHyperlink().Href(fileManagerURL).Target("_blank").Text("Browse")).
+		Else(hb.Span().Text("The URL can be base64 encoded image URL"))
+
+	input := hb.NewDiv().
+		Class(`row g-3`).
+		Style(`border: 1px solid silver;border-radius: 10px; margin-top: 0px; margin-left: 0px;margin-right: 0px;`).
+		Child(hb.NewDiv().
+			Class(`col-md-2`).
+			Child(image),
+		).
+		Child(hb.NewDiv().
+			Class(`col-md-10`).
+			Child(textArea).
+			Child(fileManagerLink),
+		)
+
+	return input
+}
+
+func (field *Field) fieldHtmlArea() *hb.Tag {
+	textarea := hb.NewTextArea().
+		ID(field.ID).
+		Class("form-control").
+		Name(field.Name).
+		Text(field.Value)
+
+	input := hb.NewWrap().
+		Child(textarea).
+		Child(hb.NewScript(field.TrumbowygScript()))
+	// Child(hb.NewScript(`setTimeout(() => {initWysiwyg("` + field.ID + `")}, 100);`))
+
+	return input
+}
+
+func (field *Field) fieldSelect() *hb.Tag {
+	input := hb.NewSelect().
+		ID(field.ID).
+		Name(field.Name).
+		Class("form-select")
+
+	for _, opt := range field.Options {
+		option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
+		option.AttrIf(field.Value == opt.Key, "selected", "selected")
+		input.AddChild(option)
+	}
+
+	if field.OptionsF != nil {
+		for _, opt := range field.OptionsF() {
+			option := hb.NewOption().Value(opt.Key).HTML(opt.Value)
+			option.AttrIf(field.Value == opt.Key, "selected", "selected")
+			input.AddChild(option)
+		}
+	}
+	return input
+}
+
+func (field *Field) fieldTable(fileManagerURL string) *hb.Tag {
+	header := hb.NewThead()
+	if field.TableOptions.RowDeleteButton != nil {
+		th := hb.NewTH().HTML("#").Style("width:1px;")
+		header.AddChild(th)
+	}
+	for _, v := range field.TableOptions.Header {
+		th := hb.NewTH().HTML(v.Label)
+		if v.Width != 0 {
+			th.Style("width:" + strconv.Itoa(v.Width) + "px")
+		}
+		header.AddChild(th)
+	}
+
+	rows := hb.NewTbody()
+	for rowIndex, rowFields := range field.TableOptions.Rows {
+		tr := hb.NewTR().Data("row-index", utils.ToString(rowIndex))
+		if field.TableOptions.RowDeleteButton != nil {
+			deleteButton := field.TableOptions.RowDeleteButton.
+				Type(hb.TYPE_BUTTON).
+				Data("row-index", utils.ToString(rowIndex))
+			td := hb.NewTH().Child(deleteButton)
+			tr.AddChild(td)
+		}
+		for _, rowField := range rowFields {
+			td := hb.NewTD().Child(rowField.fieldInput(fileManagerURL))
+			tr.AddChild(td)
+		}
+		rows.AddChild(tr)
+	}
+	table := hb.NewTable().
+		ID(field.ID).
+		Class("table table-striped table-hover mb-0").
+		Child(header).
+		Child(rows)
+
+	input := hb.NewWrap().Child(table)
+
+	if field.TableOptions.RowAddButton != nil {
+		input.AddChild(hb.NewDiv().Child(field.TableOptions.RowAddButton.Type(hb.TYPE_BUTTON)))
+	}
+
+	return input
+}
+
+func (field *Field) fieldTextArea() *hb.Tag {
+	return hb.NewTextArea().
+		ID(field.ID).
+		Type(hb.TYPE_TEXT).
+		Class("form-control").
+		Name(field.Name).
+		Value(field.Value)
 }
 
 func (field *Field) BuildFormGroup(fileManagerURL string) *hb.Tag {
